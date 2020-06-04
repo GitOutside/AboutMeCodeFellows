97867134a6b72bba246a6b00e4035f5e70e0a5f7
GitOutside <colleensexton70@gmail.com>
2020-06-04 13:45:09 -0700

    CSRL yesNO

--- js/app.js
+++ js/app.js
@@ -1,98 +1,101 @@
 'use strict';
 let count = 0;
 function UserName(){
-var userName = prompt("What's your name?")
-if (userName !== null){
-  alert('Welcome ' + userName + ', here are some questions to get to know me!');
-} else{
-  alert('I do not know your name.');
-}
+  var userName = prompt("What's your name?")
+  if (userName !== null){
+    alert('Welcome ' + userName + ', here are some questions to get to know me!');
+  } else{
+    alert('I do not know your name.');
+  }
 
-return userName;
+  return userName;
 }
 var logName = UserName();
-/*
-const questionsArray = ["truckColorQ", "houseColorQ", "hairColorQ", "birthPlaceQ", "beforeNinetiesQ"]
-for(let i = 0; i < questionsArray.length; i++ ){
-  switch(questionsArray[i])
-  {
+
+function yesNo (){
+  const questionsArray = ["truckColorQ", "houseColorQ", "hairColorQ", "birthPlaceQ", "beforeNinetiesQ"]
+  for(let i = 0; i < questionsArray.length; i++ ){
+    switch(questionsArray[i])
+    {
     case "truckColorQ":
       var truckColor = prompt('Is my truck white?');
       var truckColorLowerCase = truckColor.toLowerCase();
-        if(truckColorLowerCase === "yes" || truckColorLowerCase === "y"){
-          count++;
-          console.log('Your Answer is Correct!');
-          alert('Your Answer is Correct!')
-        } else if(truckColorLowerCase === "no" || truckColorLowerCase === "n"){
-          console.log('Your Answer is Incorrect!');
-          alert('Your Answer is Incorrect!')
-        } else{
-          alert("Not a Valid Answer!")
-        };
+      if(truckColorLowerCase === "yes" || truckColorLowerCase === "y"){
+        count++;
+        console.log('Your Answer is Correct!');
+        alert('Your Answer is Correct!')
+      } else if(truckColorLowerCase === "no" || truckColorLowerCase === "n"){
+        console.log('Your Answer is Incorrect!');
+        alert('Your Answer is Incorrect!')
+      } else{
+        alert("Not a Valid Answer!")
+      };
       break;
-      case "houseColorQ":
-        var houseColor = prompt('Is my house green?');
-        var houseColorLowerCase = houseColor.toLowerCase();
-          if(houseColorLowerCase === "yes" || houseColorLowerCase === "y"){
-            console.log('Your Answer is Incorrect!');
-            alert('Your Answer is Incorrect!')
-          } else if(houseColorLowerCase === "no" || houseColorLowerCase === "n"){
-            count++;
-            console.log('Your Answer is Correct!');
-            alert('Your Answer is Correct!')
-          } else{
-            alert("Not a Valid Answer!")
-          }
-        break;
-      case "hairColorQ":
-        var hairColor = prompt('Is my hair black?');
-        var hairColorLowerCase = hairColor.toLowerCase();
-          if(hairColorLowerCase === "yes" || hairColorLowerCase === "y"){
-            count++;
-            console.log('Your Answer is Correct!');
-            alert('Your Answer is Correct!')
-          } else if(hairColorLowerCase === "no" || hairColorLowerCase === "n"){
-            console.log('Your Answer is Incorrect!');
-            alert('Your Answer is Incorrect!')
-          } else{
-            alert("Not a Valid Answer!")
-          }
-        break;
-      case "birthPlaceQ":
-        var birthPlace = prompt('Was I born in California?');
-        var birthPlaceLowerCase = birthPlace.toLowerCase();
-          if(birthPlaceLowerCase === "yes" || birthPlaceLowerCase === "y"){
-            console.log('Your Answer is Incorrect!');
-            alert('Your Answer is Incorrect!')
-          } else if(birthPlaceLowerCase === "no" || birthPlaceLowerCase === "n"){
-            count++;
-            console.log('Your Answer is Correct!');
-            alert('Your Answer is Correct!')
-          } else{
-            alert("Not a Valid Answer!")
-          }
-        break;
-      case "beforeNinetiesQ":
-        var beforeNineties = prompt('Was I born before 1990?');
-        var beforeNinetiesLowerCase = beforeNineties.toLowerCase();
-          if(beforeNinetiesLowerCase === "yes" || beforeNinetiesLowerCase === "y"){
-            count++;
-            console.log('Your Answer is Correct!');
-            alert('Your Answer is Correct!')
-          } else if(beforeNinetiesLowerCase === "no" || beforeNinetiesLowerCase === "n"){
-            console.log('Your Answer is Incorrect!');
-            alert('Your Answer is Incorrect!')
-          } else{
-            alert("Not a Valid Answer!")
-          }
-        break;
-      default :
-          console.log("it's the end")
-        break;
+    case "houseColorQ":
+      var houseColor = prompt('Is my house green?');
+      var houseColorLowerCase = houseColor.toLowerCase();
+      if(houseColorLowerCase === "yes" || houseColorLowerCase === "y"){
+        console.log('Your Answer is Incorrect!');
+        alert('Your Answer is Incorrect!')
+      } else if(houseColorLowerCase === "no" || houseColorLowerCase === "n"){
+        count++;
+        console.log('Your Answer is Correct!');
+        alert('Your Answer is Correct!')
+      } else{
+        alert("Not a Valid Answer!")
+      }
+      break;
+    case "hairColorQ":
+      var hairColor = prompt('Is my hair black?');
+      var hairColorLowerCase = hairColor.toLowerCase();
+      if(hairColorLowerCase === "yes" || hairColorLowerCase === "y"){
+        count++;
+        console.log('Your Answer is Correct!');
+        alert('Your Answer is Correct!')
+      } else if(hairColorLowerCase === "no" || hairColorLowerCase === "n"){
+        console.log('Your Answer is Incorrect!');
+        alert('Your Answer is Incorrect!')
+      } else{
+        alert("Not a Valid Answer!")
+      }
+      break;
+    case "birthPlaceQ":
+      var birthPlace = prompt('Was I born in California?');
+      var birthPlaceLowerCase = birthPlace.toLowerCase();
+      if(birthPlaceLowerCase === "yes" || birthPlaceLowerCase === "y"){
+        console.log('Your Answer is Incorrect!');
+        alert('Your Answer is Incorrect!')
+      } else if(birthPlaceLowerCase === "no" || birthPlaceLowerCase === "n"){
+        count++;
+        console.log('Your Answer is Correct!');
+        alert('Your Answer is Correct!')
+      } else{
+        alert("Not a Valid Answer!")
+      }
+      break;
+    case "beforeNinetiesQ":
+      var beforeNineties = prompt('Was I born before 1990?');
+      var beforeNinetiesLowerCase = beforeNineties.toLowerCase();
+      if(beforeNinetiesLowerCase === "yes" || beforeNinetiesLowerCase === "y"){
+        count++;
+        console.log('Your Answer is Correct!');
+        alert('Your Answer is Correct!')
+      } else if(beforeNinetiesLowerCase === "no" || beforeNinetiesLowerCase === "n"){
+        console.log('Your Answer is Incorrect!');
+        alert('Your Answer is Incorrect!')
+      } else{
+        alert("Not a Valid Answer!")
+      }
+      break;
+    default :
+      console.log("it's the end")
+      break;
 
+    }
   }
 }
-
+yesNo ();
+/*
   let favNumber = prompt('What do you think my favorite number is? hint: try from 0 to 15')
   const correctNumber = Math.floor(Math.random() * 16);
   for(let i = 0; i <= 3; i++){
@@ -137,12 +140,12 @@ for(let i = 0; i < questionsArray.length; i++ ){
       }
     }
 
-alert('You got ' + count + ' questions correct out of 10.')
-
-if (userName !== null){
-  alert(userName + ' now you know a little about me! Here is some more information about me.')
-} else{
-  alert('I do not know your name, but here is some more information about me.');
-}
-
-*/
+    alert('You got ' + count + ' questions correct out of 10.')
+    
+    if (userName !== null){
+      alert(userName + ' now you know a little about me! Here is some more information about me.')
+    } else{
+      alert('I do not know your name, but here is some more information about me.');
+    }
+    
+    */
